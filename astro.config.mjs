148aee5ee9487922f080a401ba5d498c8d98089c
Grisha Angelov <grisha.angelov89@gmail.com>
2025-02-26 11:31:48 +0200

    migrate to astrojs v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
 import { defineConfig } from 'astro/config';
-
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 import tailwind from "@astrojs/tailwind";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
+  vite: {
+    resolve: {
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@utils': path.resolve(__dirname, 'src/utils.ts'),
+        '@images': path.resolve(__dirname, 'src/images'),
+        '@layouts': path.resolve(__dirname, 'src/layouts'),
+      }
+    }
+  }
 });
