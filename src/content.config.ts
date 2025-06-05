3dec3816b591abfc70b61818aa2833ac83129cb2
Grisha Angelov <grisha.angelov89@gmail.com>
2025-06-05 13:41:01 +0300

    Update dependencies

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,12 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
+
 const blogCollection = defineCollection({
-  type: 'content',
+  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
   schema: z.object({
     title: z.string(),
-    pubDate: z.date(),
+    pubDate: z.coerce.date(),
     author: z.string(),
     image: z.string(),
     tags: z.array(z.string()),
